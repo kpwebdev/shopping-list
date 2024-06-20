718f21dd3b92fdea158f2e519ad59645797baee7
KP Web Dev <kpwebdevishere@gmail.com>
2024-06-20 16:43:15 +0530

    starting point of the project

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("node:path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    hot: true,
+    open: true,
+    port: 5000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Shopping List",
+      filename: "index.html",
+      template: "./index.html",
+    }),
+  ],
+};
